16f52ade4e29e902ffa3019c4cc76bf242d3c8d4
Jaypatil1327 <jaypatil135790@gmail.com>
2025-12-13 21:57:27 +0530

    13-12-2025

---
+++ ts with react/React-With-Ts/src/App.tsx
@@ -0,0 +1,10 @@
+import UserDetail from "./components/user_details";
+
+function App() {
+  return;
+  <div>
+    <UserDetail id={"1"} subtitle={<p>hey I'm user first</p>} />;
+  </div>;
+}
+
+export default App;
